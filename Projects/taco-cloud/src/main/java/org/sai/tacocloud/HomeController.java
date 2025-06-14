7f6c854a2a9f55eec4fd44af9ea69533baa1070e
saikad27 <sairajkad501@icloud.com>
2025-06-14 23:46:54 +0530

    second commit

---
+++ Projects/taco-cloud/src/main/java/org/sai/tacocloud/HomeController.java
@@ -0,0 +1,21 @@
+package org.sai.tacocloud;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+
+public class HomeController {
+
+    @GetMapping("/home")
+    String home(){
+        return "home";
+    }
+
+    @GetMapping("/login")
+    String getLogin(){
+        return "login";
+    }
+
+}
