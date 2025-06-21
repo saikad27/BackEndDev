ff62b1667d0ae50eece684766e023b859cf12999
saikad27 <sairajkad501@icloud.com>
2025-06-21 15:11:34 +0530

    Rest Services Implementation

---
+++ Spring/8_REST_Services/2_sending_objects_from_server_to_client/src/test/java/com/example/__creating_a_REST_service/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.__creating_a_REST_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
